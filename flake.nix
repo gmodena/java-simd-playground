e6a1edbfcf94202a8653850b60c5dc42e32a7871
Gabriele Modena <gmodena@pm.me>
2024-05-26 21:39:02 +0200

    flake: version bump to java22

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Flake to manage a Java 21 workspace.";
+  description = "Flake to manage a Java 22 workspace.";
 
   inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
 
@@ -14,8 +14,8 @@
         buildInputs = with pkgs; [ jdk21 gradle ];
 
         shellHook = ''
-          export JAVA_HOME=${pkgs.jdk21}
-          PATH="${pkgs.jdk21}/bin:$PATH"
+          export JAVA_HOME=${pkgs.jdk22}
+          PATH="${pkgs.jdk22}/bin:$PATH"
         '';
       };
     };
